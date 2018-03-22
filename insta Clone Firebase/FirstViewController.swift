40932243d01051dba82a11310a6d27f0f7b08f99
enespolat <turksancaktar1071@gmail.com>
2018-03-22 07:32:25 -0700

    Initial Commit

---
+++ insta Clone Firebase/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  insta Clone Firebase
+//
+//  Created by enespolat on 22.03.2018.
+//  Copyright © 2018 enespolat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ insta Clone Firebase/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  insta Clone Firebase
+//
+//  Created by enespolat on 22.03.2018.
+//  Copyright © 2018 enespolat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
