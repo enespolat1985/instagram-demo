5322df259beadf7aa9323fe9cd503b4de672c173
enespolat <turksancaktar1071@gmail.com>
2018-03-26 01:04:45 -0700

    first commit

---
+++ insta Clone Firebase/feedCell.swift
@@ -0,0 +1,24 @@
+//
+//  feedCell.swift
+//  insta Clone Firebase
+//
+//  Created by enespolat on 25.03.2018.
+//  Copyright © 2018 enespolat. All rights reserved.
+//
+
+import UIKit
+
+class feedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
